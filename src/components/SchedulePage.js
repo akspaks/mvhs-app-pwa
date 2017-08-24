624a8e327a7a4a135f6b94f22b8686f9ea754387
akspaks <31290858+akspaks@users.noreply.github.com>
2017-08-23 17:31:01 -0700

    Update SchedulePage.js
    This is what I tried so far...

--- src/components/SchedulePage.js
+++ src/components/SchedulePage.js
@@ -1,10 +1,15 @@
 // @flow
 
 import React from 'react';
+import ReactDOM from 'react-dom';
 
 import BellScheduleContainer from '../containers/BellScheduleContainer';
 import DatePickerContainer from '../containers/DatePickerContainer';
 import CalendarContainer from '../containers/CalendarContainer';
+
+reactDOM.render(<p>The information may not be fully accurate or finalized due to various unforeseen reasons. 
+                It is important to double check with the school website for the official school related information.</p>,
+                document.getElementById('app'));
 
 type Props = {
   date: moment$Moment,
